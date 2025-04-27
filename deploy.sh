c0de1356c2b61c7d74a68dc17962b63f5eab2fa2
khyati-pencilz <khyati@pencilz.io>
2025-04-28 03:02:06 +0530

    fix: changed context to folders

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,13 @@ echo "============================================"
 # Load environment variables (if you have a .env file)
 export $(grep -v '^#' .env | xargs)  # Optional if needed
 
+# clone frontend
+git clone https://github.com/khyatijha777/indian-open-mic-ui.git
+
+# clone backend
+git clone https://github.com/khyatijha777/indian-open-mic-api.git
+
+
 # Build fresh images
 docker-compose build
 
