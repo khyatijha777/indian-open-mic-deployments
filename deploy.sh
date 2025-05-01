ee880c6613ef3dcc5b8e64c1f70921523c90bdba
khyati-pencilz <khyati@pencilz.io>
2025-05-01 20:10:57 +0530

    fix: pull from docker hub

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,11 @@ echo " 🚀 Starting Indian Open Mic App Deployment "
 echo "============================================"
 
 
-# Load environment variables (if you have a .env file)
-export $(grep -v '^#' .env | xargs)  # Optional if needed
+
+
+rm -rf indian-open-mic-ui indian-open-mic-api
+rm -rf indian-open-mic-ui indian-open-mic-ui
+
 
 # clone frontend
 git clone https://github.com/khyatijha777/indian-open-mic-ui.git
@@ -22,6 +25,9 @@ git clone https://github.com/khyatijha777/indian-open-mic-api.git
 
 # Download .env.production from S3
 aws s3 cp s3://indiaopenmic-media-uploads/.env .env
+
+# Load environment variables (if you have a .env file)
+export $(grep -v '^#' .env | xargs)  # Optional if needed
 
 # Build fresh images
 docker-compose build
