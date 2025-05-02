9e9e9b095a67121f3a4b49103ab7a15d93d4c945
khyati-pencilz <khyati@pencilz.io>
2025-05-02 18:05:52 +0530

    fix: cloning is not required

--- deploy.sh
+++ deploy.sh
@@ -17,11 +17,12 @@ aws s3 cp s3://indiaopenmic-media-uploads/.env .env
 # Load environment variables (if you have a .env file)
 export $(grep -v '^#' .env | xargs)  # Optional if needed
 
-# Build fresh images
-docker-compose build
-
 # Stop old containers if running
 docker-compose down
+
+# delete existing images
+docker rmi khyatijha777/indian-open-mic-ui
+docker rmi khyatijha777/indian-open-mic-api
 
 # Bring up new containers
 docker-compose up -d
